26d97888b4edb74f544e1bc480f604a78cf131ac
Mokenye <151967443+mokenye@users.noreply.github.com>
2026-04-08 17:16:30 -0400

    fix(auth): disable Google OAuth UI until custom domain is configured

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: process.env.CALLBACK_URL || "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
